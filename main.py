3048b6ba7d5fa1ae493a0c2109920c7315b59ec6
Lucas Claro <lucas-claro@hotmail.com>
2022-07-25 06:44:50 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# um arquivo pode ser aberto em modo de leitura ou de edição
+# w para editar (write)
+# r para ler (read)
+
+with open ('meuarquivo.docx ', 'w') as arquivo:
+    arquivo.write('Ola, mundo')
+
+
+# nessa segunda opção é necessário criar uma linha de código para fechar o arquivo
+# enquanto na estrutura acima, utilizando o with, o código é encerrado e o arquivo encerrado assim que sair da indentação
+#arquivo = open('meuarquivo.docx ', 'w')
+#arquivo.write('Ola, mundo!')
+#arquivo.close()
