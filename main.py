23a2f3f98f47f964419b6e4a17051fdbb8c6ad32
Lucas Claro <102222870+lucasaclaro@users.noreply.github.com>
2022-07-26 06:55:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 # w para editar (write)
 # r para ler (read)
 
-with open ('meuarquivo.docx ', 'w') as arquivo:
+with open ('meuarquivo.txt ', 'w') as arquivo:
     arquivo.write('Ola, mundo')
 
 
 # nessa segunda opção é necessário criar uma linha de código para fechar o arquivo
 # enquanto na estrutura acima, utilizando o with, o código é encerrado e o arquivo encerrado assim que sair da indentação
-#arquivo = open('meuarquivo.docx ', 'w')
+#arquivo = open('meuarquivo.txt ', 'w')
 #arquivo.write('Ola, mundo!')
 #arquivo.close()
